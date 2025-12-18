51a33ac12b58374528039651620fef533d438c2f
monalvarez <sunmonkey.business@gmail.com>
2025-12-18 08:38:55 +0100

    Añadidos nuevos endpoints en api y apitest

--- test/rest/api_test.py
+++ test/rest/api_test.py
@@ -24,6 +24,36 @@ class TestApi(unittest.TestCase):
         self.assertEqual(
             response.read().decode(), "3", "ERROR ADD"
         )
+    
+    def test_api_substract(self):
+        url = f"{BASE_URL}/calc/substract/5/2"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.OK, f"Error en la petición API a {url}"
+        )
+        self.assertEqual(
+            response.read().decode(), "3", "ERROR SUBSTRACT"
+        )
+
+    def test_api_multiply(self):
+        url = f"{BASE_URL}/calc/multiply/3/2"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.OK, f"Error en la petición API a {url}"
+        )
+        self.assertEqual(
+            response.read().decode(), "6", "ERROR MULTIPLY"
+        )
+    
+    def test_api_divide(self):
+        url = f"{BASE_URL}/calc/divide/2/2"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.OK, f"Error en la petición API a {url}"
+        )
+        self.assertEqual(
+            response.read().decode(), "1", "ERROR DIVIDE"
+        )
 
     """ def test_api_sqrt(self):
         url = f"{BASE_URL_MOCK}/calc/sqrt/64"
